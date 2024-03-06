c393e33a6f98f7b52d236700d8b6b832c9752cf1
nik <n.kostrukov@mail.ru>
2024-03-06 22:49:46 +0300

    add functional to the project

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,26 +29,80 @@
         private void InitializeComponent()
         {
             dataGridView1 = new DataGridView();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            comboBox1 = new ComboBox();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // dataGridView1
             // 
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.ColumnHeader;
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllHeaders;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(31, 35);
+            dataGridView1.Location = new Point(4, -1);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.Size = new Size(719, 244);
+            dataGridView1.Size = new Size(1166, 436);
             dataGridView1.TabIndex = 0;
+            dataGridView1.DataError += dataGridView1_DataError;
+            // 
+            // button1
+            // 
+            button1.Enabled = false;
+            button1.Location = new Point(24, 461);
+            button1.Name = "button1";
+            button1.Size = new Size(133, 46);
+            button1.TabIndex = 1;
+            button1.Text = "Выбрать";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(198, 461);
+            button2.Name = "button2";
+            button2.Size = new Size(133, 46);
+            button2.TabIndex = 2;
+            button2.Text = "Добавить";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(381, 461);
+            button3.Name = "button3";
+            button3.Size = new Size(133, 46);
+            button3.TabIndex = 3;
+            button3.Text = "Удалить";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // comboBox1
+            // 
+            comboBox1.Location = new Point(24, 513);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(155, 28);
+            comboBox1.TabIndex = 5;
+            comboBox1.Text = "Выберите таблицу";
+            comboBox1.DropDown += comboBox1_DropDown;
+            comboBox1.SelectionChangeCommitted += comboBox1_SelectionChangeCommitted;
+            comboBox1.MouseDown += comboBox1_MouseDown;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1174, 553);
+            Controls.Add(comboBox1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
             Controls.Add(dataGridView1);
+            MaximizeBox = false;
             Name = "Form1";
-            Text = "Form1";
+            Text = "База данных";
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
         }
@@ -56,5 +110,9 @@
         #endregion
 
         private DataGridView dataGridView1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private ComboBox comboBox1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,115 @@
+
+using MySql.Data.MySqlClient;
+using System.Data;
+
 namespace MyDataBase
 {
     public partial class Form1 : Form
     {
+        DB db = new DB();
+
+        MySqlDataAdapter adapter = new MySqlDataAdapter();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public DataTable GetTable(string command)
+        {
+            this.db.OpenConnection();
+
+            MySqlCommand cmd = new MySqlCommand(command, this.db.GetConnection());
+
+            DataTable table = new DataTable();
+
+            this.adapter.SelectCommand = cmd;
+            this.adapter.Fill(table);
+
+            this.db.CloseConnection();
+
+            return table;
+        }
+
+        public void AddTablesToComboBox()
+        {
+            string command = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_SCHEMA = 'online-shop'";
+            foreach (DataRow row in GetTable(command).Rows)
+            {
+                comboBox1.Items.Add(row["TABLE_NAME"]);
+            }
+        }
+
+        private void SelectDataOfTable()
+        {
+            dataGridView1.DataSource = GetTable($"SELECT * FROM {comboBox1.SelectedItem}");
+        }
+
+        private void UpdateDataOfTable()
+        {
+            DataTable? datatable = dataGridView1.DataSource as DataTable;
+
+            MySqlCommandBuilder commandBuilder = new MySqlCommandBuilder(adapter);
+
+            try
+            {
+                if (datatable != null) { adapter.Update(datatable); }
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+
+        private void DeleteDataOfTable()
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                int selectedIndex = dataGridView1.SelectedRows[0].Index;
+                try
+                {
+                    dataGridView1.Rows.RemoveAt(selectedIndex);
+                }
+                catch (InvalidOperationException) { MessageBox.Show("Нельзя удалить пустую строку"); }
+                
+
+                UpdateDataOfTable();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SelectDataOfTable();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            UpdateDataOfTable();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DeleteDataOfTable();
+        }
+
+        private void comboBox1_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+        }
+
+        private void comboBox1_DropDown(object sender, EventArgs e)
+        {
+            if (comboBox1.Items.Count == 0)
+            {
+                AddTablesToComboBox();
+            }
+        }
+
+        private void comboBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+        }
+
+        private void dataGridView1_DataError(object sender, DataGridViewDataErrorEventArgs e)
+        {
+            MessageBox.Show(e.ToString());
         }
     }
 }
